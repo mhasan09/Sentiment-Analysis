912779469059140b1387f0376efc1de9953e9804
Mahabubul Hasan <31323041+mhasan09@users.noreply.github.com>
2019-04-13 20:28:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from textblob import TextBlob
 import tweepy
-key= 'imF8YpanOMKifjbGw7MoCfB7L'
-secret= '35nTQBo2uLyyPtKlKy34FAHRrVN8RTRKqz0RDPD8YH4E9AEgZy'
-access_token = '1114391929440165888-A3medjceBaPcnXouLGZn5NKJmrjLVF'
-access_token_secret = 'QLbIXKQnQS4oOxPq9386SbenAz0BReCbvtITRUgGYYQ2S'
+key= ''
+secret= ''
+access_token = ''
+access_token_secret = ''
 auth= tweepy.OAuthHandler(key,secret)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
